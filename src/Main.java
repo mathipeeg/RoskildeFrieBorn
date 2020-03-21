141b4a03657097422ac543b7d4b57853328db9b5
unknown <casperlm@outlook.dk>
2020-03-21 16:00:50 +0100

    =Jeg tilføjede kommentar

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Mit første push!
         Child child = new Child();
         Parent parent = new Parent();
         Staff staff = new Staff();
