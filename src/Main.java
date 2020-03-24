f01d4232f1b6a3f458aefbd38d722bb40ddbdad9
mathipeeg <math_dahl@hotmail.com>
2020-03-24 14:32:44 +0100

    Newest version again

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main
 {
     public static void main(String[] args) throws ParseException {
 
+        //Nyt push
+
         Controller controller = new Controller();
         Menu menu = new Menu();
 
