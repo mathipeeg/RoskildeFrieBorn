2c1f494d8c8aaeb39ca2f27a35d096c7b86f9671
mathipeeg <math_dahl@hotmail.com>
2020-03-24 15:00:39 +0100

    delete organising

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args) throws ParseException {
 
-        //Nyt push
+        //Nyt push kl 15.00
 
         Controller controller = new Controller();
         Menu menu = new Menu();
